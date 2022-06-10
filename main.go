cbff7abd28a78a3c49edeb6e377a2031717a7bf6
karencfv <karencfv@users.noreply.github.com>
2022-06-10 15:48:39 +1200

    Fix issue where twice as many results where being returned

--- main.go
+++ main.go
@@ -5,12 +5,20 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/oxidecomputer/terraform-provider-oxide-demo/oxide"
 )
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: oxide.Provider,
+		Debug:        debug,
 	})
 }
