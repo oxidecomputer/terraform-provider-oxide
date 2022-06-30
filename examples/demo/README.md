ce41e030b602b3ea54eac32ba278c2ed38cdcb9e
Karen Cárcamo <karencfv@users.noreply.github.com>
2022-06-30 15:00:30 +1200

    Create smaller disks for demo (#28)

--- examples/demo/README.md
+++ examples/demo/README.md
@@ -7,7 +7,7 @@ This Terraform configuration file provisions 5 instances as per the demo script:
 This configuration file does the following:
 
 1. Retrieves data about the organization, project and global image. This configuration file assumes there will only be one of each and those are the ones we'll be using for the demo.
-2. Using the data retrieved from the previous step, 5 disks are created; 3 100GiB disks for the web instances and 2 500GiB disks for the DB instances.
-3. Finally, using data from the first and second steps, the 5 instances are created using the default VPC and subnet; 3 web instances with 2 CPUs and 8GiB of memory, and 2 DB instances with 8 CPUs and 32GiB of memory.
+2. Using the data retrieved from the previous step, 5 disks are created; 3 10GiB disks for the web instances and 2 20GiB disks for the DB instances.
+3. Finally, using data from the first and second steps, the 5 instances are created using the default VPC and subnet; 3 web instances with 2 CPUs and 1GiB of memory, and 2 DB instances with 4 CPUs and 2GiB of memory.
 
 To try out this configuration file follow the [instructions](https://github.com/oxidecomputer/terraform-provider-oxide-demo/#using-the-provider) from the README.
--- examples/demo/demo.tf
+++ examples/demo/demo.tf
@@ -24,7 +24,7 @@ resource "oxide_disk" "web_disk_1" {
   project_name      = data.oxide_projects.project_list.projects.0.name
   description       = "Disk for a web instance"
   name              = "web-disk-1"
-  size              = 107374182400
+  size              = 10737418240
   disk_source       = { global_image = data.oxide_global_images.image_list.global_images.0.id }
 }
 
@@ -33,7 +33,7 @@ resource "oxide_disk" "web_disk_2" {
   project_name      = data.oxide_projects.project_list.projects.0.name
   description       = "Disk for a web instance"
   name              = "web-disk-2"
-  size              = 107374182400
+  size              = 10737418240
   disk_source       = { global_image = data.oxide_global_images.image_list.global_images.0.id }
 }
 
@@ -42,7 +42,7 @@ resource "oxide_disk" "web_disk_3" {
   project_name      = data.oxide_projects.project_list.projects.0.name
   description       = "Disk for a web instance"
   name              = "web-disk-3"
-  size              = 107374182400
+  size              = 10737418240
   disk_source       = { global_image = data.oxide_global_images.image_list.global_images.0.id }
 }
 
@@ -51,7 +51,7 @@ resource "oxide_disk" "db_disk_1" {
   project_name      = data.oxide_projects.project_list.projects.0.name
   description       = "Disk for a DB instance"
   name              = "db-disk-1"
-  size              = 536870912000
+  size              = 21474836480
   disk_source       = { global_image = data.oxide_global_images.image_list.global_images.0.id }
 }
 
@@ -60,7 +60,7 @@ resource "oxide_disk" "db_disk_2" {
   project_name      = data.oxide_projects.project_list.projects.0.name
   description       = "Disk for a DB instance"
   name              = "db-disk-2"
-  size              = 536870912000
+  size              = 21474836480
   disk_source       = { global_image = data.oxide_global_images.image_list.global_images.0.id }
 }
 
@@ -70,7 +70,7 @@ resource "oxide_instance" "web_instance_1" {
   description       = "Web instance"
   name              = "web-instance-1"
   host_name         = "web-instance-1"
-  memory            = 8589934592
+  memory            = 1073741824
   ncpus             = 2
   attach_to_disks   = [oxide_disk.web_disk_1.name]
   network_interface {
@@ -87,7 +87,7 @@ resource "oxide_instance" "web_instance_2" {
   description       = "Web instance"
   name              = "web-instance-2"
   host_name         = "web-instance-2"
-  memory            = 8589934592
+  memory            = 1073741824
   ncpus             = 2
   attach_to_disks   = [oxide_disk.web_disk_2.name]
   network_interface {
@@ -104,7 +104,7 @@ resource "oxide_instance" "web_instance_3" {
   description       = "Web instance"
   name              = "web-instance-3"
   host_name         = "web-instance-3"
-  memory            = 8589934592
+  memory            = 1073741824
   ncpus             = 2
   attach_to_disks   = [oxide_disk.web_disk_3.name]
   network_interface {
@@ -121,8 +121,8 @@ resource "oxide_instance" "db_instance_1" {
   description       = "Web instance"
   name              = "db-instance-1"
   host_name         = "db-instance-1"
-  memory            = 34359738368
-  ncpus             = 8
+  memory            = 2147483648
+  ncpus             = 4
   attach_to_disks   = [oxide_disk.db_disk_1.name]
   network_interface {
     description = "Network interface for DB instance"
@@ -138,8 +138,8 @@ resource "oxide_instance" "db_instance_2" {
   description       = "Web instance"
   name              = "db-instance-2"
   host_name         = "db-instance-2"
-  memory            = 34359738368
-  ncpus             = 8
+  memory            = 2147483648
+  ncpus             = 4
   attach_to_disks   = [oxide_disk.db_disk_2.name]
   network_interface {
     description = "Network interface for DB instance"
