3f1a1e141d9a1c69d0df5a1349398fadcb65556d
karencfv <karencfv@users.noreply.github.com>
2023-04-12 19:35:09 +1200

    Clean up

--- oxide/data_source_projects.go
+++ oxide/data_source_projects.go
@@ -11,18 +11,16 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 
-	//	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	oxideSDK "github.com/oxidecomputer/oxide.go/oxide"
 )
 
-var _ datasource.DataSource = &ProjectsDataSource{}
-var _ datasource.DataSourceWithConfigure = &ProjectsDataSource{}
+var _ datasource.DataSource = (*projectsDataSource)(nil)
+var _ datasource.DataSourceWithConfigure = (*projectsDataSource)(nil)
 
-type ProjectsDataSource struct {
+type projectsDataSource struct {
 	client *oxideSDK.Client
 }
 
-// projectsDataSourceModel maps the data source schema data.
 type projectsDataSourceModel struct {
 	Projects []projectModel `tfsdk:"projects"`
 }
@@ -35,12 +33,17 @@ type projectModel struct {
 	TimeModified types.String `tfsdk:"time_modified"`
 }
 
-func (d *ProjectsDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+// NewProjectsDataSource initialises a projects datasource
+func NewProjectsDataSource() datasource.DataSource {
+	return &projectsDataSource{}
+}
+
+func (d *projectsDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
 	resp.TypeName = "oxide_projects"
 }
 
 // Configure adds the provider configured client to the data source.
-func (d *ProjectsDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
+func (d *projectsDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, _ *datasource.ConfigureResponse) {
 	if req.ProviderData == nil {
 		return
 	}
@@ -48,7 +51,7 @@ func (d *ProjectsDataSource) Configure(_ context.Context, req datasource.Configu
 	d.client = req.ProviderData.(*oxideSDK.Client)
 }
 
-func (d *ProjectsDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+func (d *projectsDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"projects": schema.ListNestedAttribute{
@@ -82,11 +85,8 @@ func (d *ProjectsDataSource) Schema(ctx context.Context, req datasource.SchemaRe
 	}
 }
 
-func (d *ProjectsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+func (d *projectsDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
 	var state projectsDataSourceModel
-
-	// Read Terraform configuration data into the model
-	//resp.Diagnostics.Append(req.Config.Get(ctx, &state)...)
 
 	// TODO: It would be preferable to us the client.Projects.ListAllPages method instead.
 	// Unfortunately, currently that method has a bug where it returns twice as many results
@@ -125,104 +125,3 @@ func (d *ProjectsDataSource) Read(ctx context.Context, req datasource.ReadReques
 		return
 	}
 }
-
-//func projectsDataSource() *schema.Resource {
-//	return &schema.Resource{
-//		ReadContext: projectsDataSourceRead,
-//		Schema:      newProjectsDataSourceSchema(),
-//		Timeouts: &schema.ResourceTimeout{
-//			Default: schema.DefaultTimeout(5 * time.Minute),
-//		},
-//	}
-//}
-
-//func newProjectsDataSourceSchema() map[string]*schema.Schema {
-//	return map[string]*schema.Schema{
-//		"projects": {
-//			Computed:    true,
-//			Type:        schema.TypeList,
-//			Description: "A list of all projects",
-//			Elem: &schema.Resource{
-//				Schema: map[string]*schema.Schema{
-//					"description": {
-//						Type:        schema.TypeString,
-//					Description: "Description for the project.",
-//					Computed:    true,
-//				},
-//				"id": {
-//						Type:        schema.TypeString,
-//						Description: "Unique, immutable, system-controlled identifier of the project.",
-//						Computed:    true,
-//					},
-//					"name": {
-//						Type:        schema.TypeString,
-//						Description: "Name of the project.",
-//						Computed:    true,
-//					},
-//					"time_created": {
-//						Type:        schema.TypeString,
-//						Description: "Timestamp of when this project was created.",
-//						Computed:    true,
-//					},
-//					"time_modified": {
-//						Type:        schema.TypeString,
-//						Description: "Timestamp of when this project was last modified.",
-//						Computed:    true,
-//					},
-//				},
-//			},
-//		},
-//	}
-//}
-
-//func projectsDataSourceRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-//	client := meta.(*oxideSDK.Client)
-//
-//	// TODO: It would be preferable to us the client.Projectss.ListAllPages method instead.
-//	// Unfortunately, currently that method has a bug where it returns twice as many results
-//	// as there are in reality. For now I'll use the List method with a limit of 1,000,000 results.
-//	// Seems unlikely anyone will have more than one billion projects.
-//	params := oxideSDK.ProjectListParams{
-//		Limit:  1000000000,
-//		SortBy: oxideSDK.NameOrIdSortModeIdAscending,
-//	}
-//	result, err := client.ProjectList(params)
-//	if err != nil {
-//		return diag.FromErr(err)
-//	}
-//
-//	d.SetId(strconv.Itoa(schema.HashString(time.Now().String())))
-//
-//	if err := projectsToState(d, result); err != nil {
-//		return diag.FromErr(err)
-//	}
-//
-//	return nil
-//}
-
-//func projectsToState(d *schema.ResourceData, projects *oxideSDK.ProjectResultsPage) error {
-//	if projects == nil {
-//		return nil
-//	}
-//
-//	var result = make([]interface{}, 0, len(projects.Items))
-//	for _, project := range projects.Items {
-//		var m = make(map[string]interface{})
-//
-//		m["description"] = project.Description
-//		m["id"] = project.Id
-//		m["name"] = project.Name
-//		m["time_created"] = project.TimeCreated.String()
-//		m["time_modified"] = project.TimeModified.String()
-//
-//		result = append(result, m)
-//
-//		if len(result) > 0 {
-//			if err := d.Set("projects", result); err != nil {
-//				return err
-//			}
-//		}
-//	}
-//
-//	return nil
-//}
--- oxide/provider.go
+++ oxide/provider.go
@@ -17,30 +17,29 @@ import (
 	oxideSDK "github.com/oxidecomputer/oxide.go/oxide"
 )
 
-var _ provider.Provider = (*OxideProvider)(nil)
+var _ provider.Provider = (*oxideProvider)(nil)
 
-type OxideProvider struct {
-	// TODO: Necesito esta madre?
-	//client *oxideSDK.Client
+type oxideProvider struct {
+	// TODO: Handle version
 	//	version string
 }
 
-type OxideProviderModel struct {
+type oxideProviderModel struct {
 	Host  types.String `tfsdk:"host"`
 	Token types.String `tfsdk:"token"`
 }
 
 func New() provider.Provider {
-	return &OxideProvider{}
+	return &oxideProvider{}
 }
 
-func (p *OxideProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+func (p *oxideProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
 	resp.TypeName = "oxide"
 	// resp.Version = p.version
 }
 
 // Schema defines the provider-level schema for configuration data.
-func (p *OxideProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
+func (p *oxideProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"host": schema.StringAttribute{
@@ -57,11 +56,11 @@ func (p *OxideProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp
 }
 
 // Configure prepares the Oxide client for data sources and resources.
-func (p *OxideProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+func (p *oxideProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
 	host := os.Getenv("OXIDE_HOST")
 	token := os.Getenv("OXIDE_TOKEN")
 
-	var data OxideProviderModel
+	var data oxideProviderModel
 
 	// Read configuration data into model
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
@@ -107,87 +106,26 @@ func (p *OxideProvider) Configure(ctx context.Context, req provider.ConfigureReq
 		return
 	}
 
-	//p.client = client
 	resp.DataSourceData = client
 	resp.ResourceData = client
 }
 
 // DataSources defines the data sources implemented in the provider.
-func (p *OxideProvider) DataSources(_ context.Context) []func() datasource.DataSource {
+func (p *oxideProvider) DataSources(_ context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
-		func() datasource.DataSource { return &ProjectsDataSource{} },
-		//func() datasource.DataSource { return &imagesDataSource{} },
+		NewProjectsDataSource,
+		//NewImagesDataSource,
 	}
 }
 
 // Resources defines the resources implemented in the provider.
-func (p *OxideProvider) Resources(_ context.Context) []func() resource.Resource {
+func (p *oxideProvider) Resources(_ context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		// func() resource.Resource { return &diskResource{} },
-		// func() resource.Resource { return &imageResource{} },
-		// func() resource.Resource { return &instanceResource{} },
-		// func() resource.Resource { return &ipPoolResource{} },
-		// func() resource.Resource { return &projectResource{} },
-		// func() resource.Resource { return &vpcResource{} },
+		// NewDiskResource,
+		// NewImageResource,
+		// NewInstanceResource,
+		// NewIpPoolResource,
+		// NewProjectResource,
+		// NewVpcResource,
 	}
 }
-
-//// Provider is the schema for the oxide terraform provider
-//func Provider() *schema.Provider {
-//	return &schema.Provider{
-//		ConfigureContextFunc: newProviderMeta,
-//		Schema: map[string]*schema.Schema{
-//			"host": {
-//				Description: "URL of the root of the target server",
-//				Type:        schema.TypeString,
-//				Optional:    true,
-//				// We'll remove this validation for now. Will confirm later if needed with rack testing.
-//				// ValidateFunc: validation.IsURLWithScheme([]string{"http", "https"}),
-//				DefaultFunc: schema.MultiEnvDefaultFunc(
-//					[]string{"OXIDE_HOST", "OXIDE_TEST_HOST"}, "",
-//				),
-//			},
-//			"token": {
-//				Description: "Token used to authenticate",
-//				Type:        schema.TypeString,
-//				Optional:    true,
-//				Sensitive:   true,
-//				DefaultFunc: schema.MultiEnvDefaultFunc(
-//					// TODO: Decide on these tokens
-//					[]string{"OXIDE_TOKEN", "OXIDE_TEST_TOKEN"}, "",
-//				),
-//			},
-//		},
-//		ResourcesMap: map[string]*schema.Resource{
-//			"oxide_disk":     diskResource(),
-//			"oxide_image":    imageResource(),
-//			"oxide_instance": instanceResource(),
-//			"oxide_ip_pool":  ipPoolResource(),
-//			"oxide_project":  projectResource(),
-//			"oxide_vpc":      vpcResource(),
-//		},
-//		DataSourcesMap: map[string]*schema.Resource{
-//			"oxide_projects": projectsDataSource(),
-//			"oxide_images":   imagesDataSource(),
-//		},
-//	}
-//}
-
-//func newProviderMeta(_ context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-//	host := d.Get("host").(string)
-//	if host == "" {
-//		return nil, diag.FromErr(fmt.Errorf("host must not be empty"))
-//	}
-//
-//	token := d.Get("token").(string)
-//	if token == "" {
-//		return nil, diag.FromErr(fmt.Errorf("token must not be empty"))
-//	}
-
-//	client, err := oxideSDK.NewClient(token, "terraform-provider-oxide", host)
-//	if err != nil {
-//		return nil, diag.FromErr(err)
-//	}
-//
-//return client, nil
-//}
