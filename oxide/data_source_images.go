f4f7e4a86fa9aa038628f2b5dbc058d2c5a30b55
karencfv <karencfv@users.noreply.github.com>
2023-04-14 10:07:17 +1200

    Migrate ImageResource

--- oxide/data_source_images.go
+++ oxide/data_source_images.go
@@ -15,41 +15,43 @@ import (
 	oxideSDK "github.com/oxidecomputer/oxide.go/oxide"
 )
 
-var _ datasource.DataSource = (*imagesDataSource)(nil)
-var _ datasource.DataSourceWithConfigure = (*imagesDataSource)(nil)
+var (
+	_ datasource.DataSource              = (*imagesDataSource)(nil)
+	_ datasource.DataSourceWithConfigure = (*imagesDataSource)(nil)
+)
+
+// NewImagesDataSource initialises an images datasource
+func NewImagesDataSource() datasource.DataSource {
+	return &imagesDataSource{}
+}
 
 type imagesDataSource struct {
 	client *oxideSDK.Client
 }
 
 type imagesDataSourceModel struct {
-	ID        types.String `tfsdk:"id"`
-	ProjectID types.String `tfsdk:"project_id"`
-	Images    []imageModel `tfsdk:"images"`
+	ID        types.String           `tfsdk:"id"`
+	ProjectID types.String           `tfsdk:"project_id"`
+	Images    []imageDataSourceModel `tfsdk:"images"`
 }
 
-type imageModel struct {
-	BlockSize    types.Int64   `tfsdk:"block_size"`
-	Description  types.String  `tfsdk:"description"`
-	Digest       []digestModel `tfsdk:"digest"`
-	ID           types.String  `tfsdk:"id"`
-	Name         types.String  `tfsdk:"name"`
-	OS           types.String  `tfsdk:"os"`
-	Size         types.Int64   `tfsdk:"size"`
-	TimeCreated  types.String  `tfsdk:"time_created"`
-	TimeModified types.String  `tfsdk:"time_modified"`
-	URL          types.String  `tfsdk:"url"`
-	Version      types.String  `tfsdk:"version"`
+type imageDataSourceModel struct {
+	BlockSize    types.Int64                  `tfsdk:"block_size"`
+	Description  types.String                 `tfsdk:"description"`
+	Digest       []imageDataSourceDigestModel `tfsdk:"digest"`
+	ID           types.String                 `tfsdk:"id"`
+	Name         types.String                 `tfsdk:"name"`
+	OS           types.String                 `tfsdk:"os"`
+	Size         types.Int64                  `tfsdk:"size"`
+	TimeCreated  types.String                 `tfsdk:"time_created"`
+	TimeModified types.String                 `tfsdk:"time_modified"`
+	URL          types.String                 `tfsdk:"url"`
+	Version      types.String                 `tfsdk:"version"`
 }
 
-type digestModel struct {
+type imageDataSourceDigestModel struct {
 	Type  types.String `tfsdk:"type"`
 	Value types.String `tfsdk:"value"`
-}
-
-// NewImagesDataSource initialises an images datasource
-func NewImagesDataSource() datasource.DataSource {
-	return &imagesDataSource{}
 }
 
 func (d *imagesDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
@@ -147,6 +149,9 @@ func (d *imagesDataSource) Read(ctx context.Context, req datasource.ReadRequest,
 
 	// Read Terraform configuration data into the model
 	resp.Diagnostics.Append(req.Config.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 
 	// TODO: It would be preferable to us the client.Images.ListAllPages method instead.
 	// Unfortunately, currently that method has a bug where it returns twice as many results
@@ -161,7 +166,7 @@ func (d *imagesDataSource) Read(ctx context.Context, req datasource.ReadRequest,
 	if err != nil {
 		resp.Diagnostics.AddError(
 			"Unable to read images:",
-			err.Error(),
+			"API error: "+err.Error(),
 		)
 		return
 	}
@@ -171,7 +176,7 @@ func (d *imagesDataSource) Read(ctx context.Context, req datasource.ReadRequest,
 
 	// Map response body to model
 	for _, image := range images.Items {
-		imageState := imageModel{
+		imageState := imageDataSourceModel{
 			BlockSize:    types.Int64Value(int64(image.BlockSize)),
 			Description:  types.StringValue(image.Description),
 			ID:           types.StringValue(image.Id),
@@ -184,7 +189,7 @@ func (d *imagesDataSource) Read(ctx context.Context, req datasource.ReadRequest,
 			Version:      types.StringValue(image.Version),
 		}
 
-		digestState := digestModel{
+		digestState := imageDataSourceDigestModel{
 			Type:  types.StringValue(string(image.Digest.Type)),
 			Value: types.StringValue(image.Digest.Value),
 		}
@@ -201,161 +206,3 @@ func (d *imagesDataSource) Read(ctx context.Context, req datasource.ReadRequest,
 		return
 	}
 }
-
-//func imagesDataSource() *schema.Resource {
-//	return &schema.Resource{
-//		ReadContext: imagesDataSourceRead,
-//		Schema:      newImagesDataSourceSchema(),
-//		Timeouts: &schema.ResourceTimeout{
-//			Default: schema.DefaultTimeout(5 * time.Minute),
-//		},
-//	}
-//}
-
-//func newImagesDataSourceSchema() map[string]*schema.Schema {
-//	return map[string]*schema.Schema{
-//		"image_id": {
-//			Type:        schema.TypeString,
-//			Description: "ID of the image that contains the images.",
-//			Required:    true,
-//		},
-//		"images": {
-//			Computed:    true,
-//			Type:        schema.TypeList,
-//			Description: "A list of all images belonging to a image",
-//			Elem: &schema.Resource{
-//				Schema: map[string]*schema.Schema{
-//					"block_size": {
-//						Type:        schema.TypeInt,
-//						Description: "Block size in bytes.",
-//						Computed:    true,
-//					},
-//					"description": {
-//						Type:        schema.TypeString,
-//						Description: "Description of the image.",
-//						Computed:    true,
-//					},
-//					"digest": {
-//						Type:        schema.TypeMap,
-//						Description: "Hash of the image contents, if applicable.",
-//						Computed:    true,
-//						Elem: &schema.Schema{
-//							Type: schema.TypeString,
-//						},
-//					},
-//					"os": {
-//						Type:        schema.TypeString,
-//						Description: "OS image distribution.",
-//						Computed:    true,
-//					},
-//					"id": {
-//						Type:        schema.TypeString,
-//						Description: "Unique, immutable, system-controlled identifier for the image.",
-//						Computed:    true,
-//					},
-//					"name": {
-//						Type:        schema.TypeString,
-//						Description: "Name of the image.",
-//						Computed:    true,
-//					},
-//					"size": {
-//						Type:        schema.TypeInt,
-//						Description: "Size of the image in bytes.",
-//						Computed:    true,
-//					},
-//					"time_created": {
-//						Type:        schema.TypeString,
-//						Description: "Timestamp of when this image was created.",
-//						Computed:    true,
-//					},
-//					"time_modified": {
-//						Type:        schema.TypeString,
-//						Description: "Timestamp of when this image was last modified.",
-//						Computed:    true,
-//					},
-//					"url": {
-//						Type:        schema.TypeString,
-//						Description: "URL source of this image, if any.",
-//						Computed:    true,
-//					},
-//					"version": {
-//						Type:        schema.TypeString,
-//						Description: "Version of the OS.",
-//						Computed:    true,
-//					},
-//				},
-//			},
-//		},
-//	}
-//}
-//
-//func imagesDataSourceRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-//	client := meta.(*oxideSDK.Client)
-//	imageId := d.Get("image_id").(string)
-//
-// TODO: It would be preferable to us the client.Images.GlobalListAllPages method instead.
-// Unfortunately, currently that method has a bug where it returns twice as many results
-// as there are in reality. For now I'll use the List method with a limit of 1,000,000 results.
-// Seems unlikely anyone will have more than one billion Images.
-//	params := oxideSDK.ImageListParams{
-//		Image: oxideSDK.NameOrId(imageId),
-//		Limit:   1000000000,
-//		SortBy:  oxideSDK.NameOrIdSortModeIdAscending,
-//	}
-//	result, err := client.ImageList(params)
-//	if err != nil {
-//		return diag.FromErr(err)
-//	}
-//
-//	d.SetId(strconv.Itoa(schema.HashString(time.Now().String())))
-//
-//	if err := ImagesToState(d, result); err != nil {
-//		return diag.FromErr(err)
-//	}
-//
-//	return nil
-//}
-
-//func ImagesToState(d *schema.ResourceData, images *oxideSDK.ImageResultsPage) error {
-//	if images == nil {
-//		return nil
-//	}
-//
-//	var result = make([]interface{}, 0, len(images.Items))
-//	for _, image := range images.Items {
-//		var m = make(map[string]interface{})
-//
-//		m["block_size"] = image.BlockSize
-//		m["description"] = image.Description
-//		m["os"] = image.Os
-//		m["id"] = image.Id
-//		m["name"] = image.Name
-//		m["size"] = image.Size
-//		m["time_created"] = image.TimeCreated.String()
-//		m["time_modified"] = image.TimeModified.String()
-//		m["url"] = image.Url
-//		m["version"] = image.Version
-//
-//		if digestFlattened := flattenDigest(image.Digest); digestFlattened != nil {
-//			m["digest"] = digestFlattened
-//		}
-//
-//		result = append(result, m)
-//
-//		if len(result) > 0 {
-//			if err := d.Set("images", result); err != nil {
-//				return err
-//			}
-//		}
-//	}
-//
-//	return nil
-//}
-//
-//func flattenDigest(digest oxideSDK.Digest) map[string]interface{} {
-//	var result = make(map[string]interface{})
-//	if digest.Type != "" {
-//		result[string(digest.Type)] = digest.Value
-//	}
-//	return result
-//}
--- oxide/provider.go
+++ oxide/provider.go
@@ -111,6 +111,8 @@ func (p *oxideProvider) Configure(ctx context.Context, req provider.ConfigureReq
 	resp.ResourceData = client
 }
 
+// TODO: Implement custom timeouts with https://github.com/hashicorp/terraform-plugin-framework-timeouts
+
 // DataSources defines the data sources implemented in the provider.
 func (p *oxideProvider) DataSources(_ context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
@@ -123,7 +125,7 @@ func (p *oxideProvider) DataSources(_ context.Context) []func() datasource.DataS
 func (p *oxideProvider) Resources(_ context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		// NewDiskResource,
-		// NewImageResource,
+		NewImageResource,
 		// NewInstanceResource,
 		// NewIpPoolResource,
 		// NewProjectResource,
