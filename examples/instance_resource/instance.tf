b4470daaddd442f4df4c468df3e70fb90f34c6e6
karencfv <karencfv@users.noreply.github.com>
2023-05-26 16:51:31 +1200

    Add example of full instance config

--- examples/instance_resource/instance.tf
+++ examples/instance_resource/instance.tf
@@ -11,13 +11,44 @@ terraform {
 
 provider "oxide" {}
 
-data "oxide_projects" "project_list" {}
+data "oxide_project" "example" {
+  name = "terraform-test"
+}
 
-resource "oxide_instance" "example" {
-  project_id        = element(tolist(data.oxide_projects.project_list.projects[*].id), 0)
-  description       = "a test instance"
-  name              = "myinstance"
-  host_name         = "myhost"
-  memory            = 1073741824
-  ncpus             = 1
+data "oxide_vpc" "example" {
+  project_name = "terraform-test"
+  name         = "default"
+}
+
+data "oxide_vpc_subnet" "example" {
+  project_name = "terraform-test"
+  vpc_name     = "default"
+  name         = "default"
+}
+
+resource "oxide_disk" "example" {
+  project_id  = data.oxide_project.example.id
+  description = "a test disk"
+  name        = "terraform-disk-test"
+  size        = 1073741824
+  block_size  = 512
+}
+
+resource "oxide_instance" "test" {
+  project_id       = data.oxide_project.example.id
+  description      = "a test instance"
+  name             = "terraform-instance-test"
+  host_name        = "terraform-acc-myhost"
+  memory           = 1073741824
+  ncpus            = 1
+  start_on_create  = false
+  disk_attachments = [oxide_disk.example.id]
+  network_interfaces = [
+    {
+      subnet_id   = data.oxide_vpc_subnet.example.id
+      vpc_id      = data.oxide_vpc.example.id
+      description = "a sample nic"
+      name        = "mynic"
+    },
+  ]
 }
