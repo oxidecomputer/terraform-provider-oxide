d368045ff25870a7b8dc71a96e3e628477a15036
karencfv <karencfv@users.noreply.github.com>
2024-09-12 18:15:51 +1200

    oxide_ip_pool_silo_link resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -150,6 +150,7 @@ func (p *oxideProvider) Resources(_ context.Context) []func() resource.Resource 
 		NewImageResource,
 		NewInstanceResource,
 		NewIPPoolResource,
+		NewIpPoolSiloLinkResource,
 		NewProjectResource,
 		NewSnapshotResource,
 		NewSSHKeyResource,
