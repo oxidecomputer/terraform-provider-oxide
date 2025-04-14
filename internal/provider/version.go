1987cd03a198da96ce57a7dba5921817e6ff1428
karencfv <karencfv@users.noreply.github.com>
2025-04-15 11:37:47 +1200

    Version bump to 0.9.0

--- internal/provider/version.go
+++ internal/provider/version.go
@@ -7,4 +7,4 @@ package provider
 // TODO: Make sure this variable is updated every time a new version is released
 
 // Version contains the current terraform provider version.
-const Version = "0.8.0"
+const Version = "0.9.0"
