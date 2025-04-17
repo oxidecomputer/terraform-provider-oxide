20d78c817524ff47f818db4f18cb9c8e610390c6
Levon Tarver <ask.levon@gmail.com>
2025-04-17 14:10:19 -0500

    WIP: initial implementation for switch port settings

---
+++ internal/provider/resource_switch_port_settings_test.go
@@ -0,0 +1,11 @@
+package provider
+
+import (
+	_ "context"
+	_ "fmt"
+	_ "testing"
+
+	_ "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	_ "github.com/hashicorp/terraform-plugin-testing/terraform"
+	_ "github.com/oxidecomputer/oxide.go/oxide"
+)
