712c6ca6a973be5fdc8529dc4b556742734f0a28
Matthew Sanabria <matthew.sanabria@oxide.computer>
2025-07-09 20:04:39 -0400

    misc: bump version to v0.13.0
    Bumped the version to v0.13.0 after releasing v0.12.0 in
    https://github.com/oxidecomputer/terraform-provider-oxide/issues/462.

--- internal/provider/version.go
+++ internal/provider/version.go
@@ -7,4 +7,4 @@ package provider
 // TODO: Make sure this variable is updated every time a new version is released
 
 // Version contains the current terraform provider version.
-const Version = "0.12.0"
+const Version = "0.13.0"
