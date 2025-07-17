f0c6498284250edcfa0e69159f7c58083fd384dd
Matthew Sanabria <matthew.sanabria@oxide.computer>
2025-07-16 22:00:47 -0400

    resource(switch_port_settings): update acceptance tests

--- internal/provider/resource_switch_port_settings_test.go
+++ internal/provider/resource_switch_port_settings_test.go
@@ -14,8 +14,134 @@ import (
 	"github.com/oxidecomputer/oxide.go/oxide"
 )
 
-func TestAccSwitchPortSettings_full(t *testing.T) {
-	resourceName := "oxide_switch_port_settings.test"
+func TestAccCloudResourceSwitchPortSettings_full(t *testing.T) {
+	type resourceSwitchPortSettingsConfig struct {
+		BlockName              string
+		SwitchPortSettingsName string
+		AddressLotID           string
+	}
+
+	initialConfigTmpl := `
+resource "oxide_switch_port_settings" "{{.BlockName}}" {
+  name        = "{{.SwitchPortSettingsName}}"
+  description = "Terraform acceptance testing."
+
+  port_config = {
+    geometry = "qsfp28x1"
+  }
+
+  addresses = [
+    {
+      link_name = "phy0"
+      addresses = [
+        {
+          address        = "0.0.0.0/0"
+          address_lot_id = "{{.AddressLotID}}"
+        },
+      ]
+    },
+  ]
+
+  links = [
+    {
+      link_name = "phy0"
+      autoneg   = false
+      mtu       = 1500
+      speed     = "speed1_g"
+      lldp = {
+        enabled = false
+      }
+    },
+  ]
+
+  routes = [
+    {
+      link_name = "phy0"
+      routes = [
+        {
+          dst = "0.0.0.0/0"
+          gw  = "0.0.0.0"
+        },
+      ]
+    },
+  ]
+}
+`
+	updateConfigTmpl := `
+resource "oxide_switch_port_settings" "{{.BlockName}}" {
+  name        = "{{.SwitchPortSettingsName}}"
+  description = "Terraform acceptance testing (updated)."
+
+  port_config = {
+    geometry = "qsfp28x1"
+  }
+
+  addresses = [
+    {
+      link_name = "phy0"
+      addresses = [
+        {
+          address        = "0.0.0.0/0"
+          address_lot_id = "{{.AddressLotID}}"
+        },
+      ]
+    },
+  ]
+
+  links = [
+    {
+      link_name = "phy0"
+      autoneg   = false
+      mtu       = 1500
+      speed     = "speed1_g"
+      lldp = {
+        enabled = true
+      }
+    },
+  ]
+
+  routes = [
+    {
+      link_name = "phy0"
+      routes = [
+        {
+          dst = "0.0.0.0/0"
+          gw  = "0.0.0.0"
+        },
+      ]
+    },
+  ]
+}
+`
+
+	switchPortSettingsName := newResourceName()
+	blockName := newBlockName("switch-port-settings")
+	addressLotID := "a7d5849e-c017-4af8-a245-fb3d48fc9912"
+	resourceName := fmt.Sprintf("oxide_switch_port_settings.%s", blockName)
+
+	initialConfig, err := parsedAccConfig(
+		resourceSwitchPortSettingsConfig{
+			BlockName:              blockName,
+			SwitchPortSettingsName: switchPortSettingsName,
+			AddressLotID:           addressLotID,
+		},
+		initialConfigTmpl,
+	)
+	if err != nil {
+		t.Errorf("error parsing initial config template data: %e", err)
+	}
+
+	updateConfig, err := parsedAccConfig(
+		resourceSwitchPortSettingsConfig{
+			BlockName:              blockName,
+			SwitchPortSettingsName: switchPortSettingsName,
+			AddressLotID:           addressLotID,
+		},
+		updateConfigTmpl,
+	)
+	if err != nil {
+		t.Errorf("error parsing update config template data: %e", err)
+	}
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:                 func() { testAccPreCheck(t) },
@@ -23,21 +149,16 @@ func TestAccSwitchPortSettings_full(t *testing.T) {
 		CheckDestroy:             testAccSwitchPortSettingsDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testResourceSwitchPortSettingsCreate,
-				Check:  checkResourceSwitchPortSettings(resourceName),
+				Config: initialConfig,
+				Check:  checkResourceSwitchPortSettings(resourceName, switchPortSettingsName),
 			},
 			{
-				Config: testResourceSwitchPortSettingsUpdate,
-				Check:  checkResourceSwitchPortSettingsUpdate(resourceName),
+				Config: updateConfig,
+				Check:  checkResourceSwitchPortSettingsUpdate(resourceName, switchPortSettingsName),
 			},
 			{
-				ResourceName:      resourceName,
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				Config: testResourceSwitchPortSettingsRemoveUpdate,
-				Check:  checkResourceSwitchPortSettingsRemoveUpdate(resourceName),
+				Config: initialConfig,
+				Check:  checkResourceSwitchPortSettings(resourceName, switchPortSettingsName),
 			},
 			{
 				ResourceName:      resourceName,
@@ -48,261 +169,91 @@ func TestAccSwitchPortSettings_full(t *testing.T) {
 	})
 }
 
-var testResourceSwitchPortSettingsCreate = `
-resource "oxide_switch_port_configuration" "test" {
-  name        = "test-uplink"
-  description = "test uplink configuration"
-  port_config = "qsfp28x1"
-
-  addresses = [
-    {
-      addresses = [
-        {
-          address = "172.20.250.10/24"
-          address_lot = {
-            name = "testing"
-          }
-        },
-      ]
-      link_name = "phy0"
-    },
-  ]
-
-  # bgp_peers = []
-
-  links = [
-    {
-      autoneg = false
-      fec     = "none"
-      mtu     = 1500
-      name    = "phy0"
-      speed   = "speed40_g"
-      lldp = {
-        enabled = false
-      }
-    },
-  ]
-
-  routes = [
-    {
-      link_name = "phy0"
-      routes = [
-        {
-          destination = "0.0.0.0/0"
-          gateway     = "172.20.250.1"
-        },
-      ]
-    },
-  ]
-
-  timeouts = {
-    read   = "1m"
-    create = "3m"
-    delete = "2m"
-    update = "4m"
-  }
-}
-`
-
-func checkResourceSwitchPortSettings(resourceName string) resource.TestCheckFunc {
+func checkResourceSwitchPortSettings(resourceName string, name string) resource.TestCheckFunc {
 	return resource.ComposeAggregateTestCheckFunc([]resource.TestCheckFunc{
 		resource.TestCheckResourceAttrSet(resourceName, "id"),
-		resource.TestCheckResourceAttr(resourceName, "name", "test-uplink"),
-		resource.TestCheckResourceAttr(resourceName, "description", "test uplink configuration"),
-		resource.TestCheckResourceAttr(resourceName, "port_config", "qsfp28x1"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.link_name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address", "172.20.250.10/24"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address_lot.name", "test-address-lot"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.autoneg", "false"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.fec", "none"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.mtu", "1500"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.speed", "speed40_g"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.lldp.enabled", "false"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.link_name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.destination", "0.0.0.0/0"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.gateway", "172.20.250.1"),
-
+		resource.TestCheckResourceAttr(resourceName, "name", name),
+		resource.TestCheckResourceAttr(resourceName, "description", "Terraform acceptance testing."),
+		resource.TestCheckResourceAttr(resourceName, "port_config.geometry", "qsfp28x1"),
+		resource.TestCheckNoResourceAttr(resourceName, "bgp_peers"),
+		resource.TestCheckNoResourceAttr(resourceName, "groups"),
+		resource.TestCheckNoResourceAttr(resourceName, "interfaces"),
 		resource.TestCheckResourceAttrSet(resourceName, "time_created"),
 		resource.TestCheckResourceAttrSet(resourceName, "time_modified"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.read", "1m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.delete", "2m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.create", "3m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.update", "4m"),
+
+		resource.TestCheckResourceAttr(resourceName, "addresses.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "addresses.0.link_name", "phy0"),
+		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address", "0.0.0.0/0"),
+		resource.TestCheckResourceAttrSet(resourceName, "addresses.0.addresses.0.address_lot_id"),
+		resource.TestCheckNoResourceAttr(resourceName, "addresses.0.addresses.0.vlan_id"),
+
+		resource.TestCheckResourceAttr(resourceName, "links.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.link_name", "phy0"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.autoneg", "false"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.fec"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.mtu", "1500"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.speed", "speed1_g"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.lldp.%", "7"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.lldp.enabled", "false"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.chassis_id"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.link_description"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.link_name"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.management_ip"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.system_description"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.system_name"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.tx_eq"),
+
+		resource.TestCheckResourceAttr(resourceName, "routes.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.link_name", "phy0"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.dst", "0.0.0.0/0"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.gw", "0.0.0.0"),
+		resource.TestCheckNoResourceAttr(resourceName, "routes.0.routes.0.rib_priority"),
 	}...)
 }
 
-var testResourceSwitchPortSettingsUpdate = `
-resource "oxide_switch_port_configuration" "test" {
-  name        = "test-uplink-v2"
-  description = "test uplink configuration with additional route"
-  port_config = "qsfp28x1"
-
-  addresses = [
-    {
-      addresses = [
-        {
-          address = "172.20.250.10/24"
-          address_lot = {
-            name = "test-address-lot"
-          }
-        },
-      ]
-      link_name = "phy0"
-    },
-  ]
-
-  # bgp_peers = []
-
-  links = [
-    {
-      autoneg = false
-      fec     = "none"
-      mtu     = 1500
-      name    = "phy0"
-      speed   = "speed40_g"
-      lldp = {
-        enabled = false
-      }
-    },
-  ]
-
-  routes = [
-    {
-      link_name = "phy0"
-      routes = [
-        {
-          destination = "0.0.0.0/0"
-          gateway     = "172.20.250.1"
-        },
-        {
-          destination = "1.1.1.1/32"
-          gateway     = "172.20.250.1"
-        },
-      ]
-    },
-  ]
-
-  timeouts = {
-    read   = "1m"
-    create = "3m"
-    delete = "2m"
-    update = "4m"
-  }
-}
-`
-
-func checkResourceSwitchPortSettingsUpdate(resourceName string) resource.TestCheckFunc {
+func checkResourceSwitchPortSettingsUpdate(resourceName string, name string) resource.TestCheckFunc {
 	return resource.ComposeAggregateTestCheckFunc([]resource.TestCheckFunc{
 		resource.TestCheckResourceAttrSet(resourceName, "id"),
-		resource.TestCheckResourceAttr(resourceName, "name", "test-uplink"),
-		resource.TestCheckResourceAttr(resourceName, "description", "test uplink configuration"),
-		resource.TestCheckResourceAttr(resourceName, "port_config", "qsfp28x1"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.link_name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address", "172.20.250.10/24"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address_lot.name", "test-address-lot"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.autoneg", "false"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.fec", "none"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.mtu", "1500"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.speed", "speed40_g"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.lldp.enabled", "false"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.link_name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.destination", "0.0.0.0/0"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.gateway", "172.20.250.1"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.1.destination", "1.1.1.1/0"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.1.gateway", "172.20.250.1"),
-
+		resource.TestCheckResourceAttr(resourceName, "name", name),
+		resource.TestCheckResourceAttr(resourceName, "description", "Terraform acceptance testing (updated)."),
+		resource.TestCheckResourceAttr(resourceName, "port_config.geometry", "qsfp28x1"),
+		resource.TestCheckNoResourceAttr(resourceName, "bgp_peers"),
+		resource.TestCheckNoResourceAttr(resourceName, "groups"),
+		resource.TestCheckNoResourceAttr(resourceName, "interfaces"),
 		resource.TestCheckResourceAttrSet(resourceName, "time_created"),
 		resource.TestCheckResourceAttrSet(resourceName, "time_modified"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.read", "1m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.delete", "2m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.create", "3m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.update", "4m"),
-	}...)
-}
 
-var testResourceSwitchPortSettingsRemoveUpdate = `
-resource "oxide_switch_port_configuration" "test" {
-  name        = "test-uplink-v3"
-  description = "test uplink configuration with route removed"
-  port_config = "qsfp28x1"
-
-  addresses = [
-    {
-      addresses = [
-        {
-          address = "172.20.250.10/24"
-          address_lot = {
-            name = "test-address-lot"
-          }
-        },
-      ]
-      link_name = "phy0"
-    },
-  ]
-
-  # bgp_peers = []
-
-  links = [
-    {
-      autoneg = false
-      fec     = "none"
-      mtu     = 1500
-      name    = "phy0"
-      speed   = "speed40_g"
-      lldp = {
-        enabled = false
-      }
-    },
-  ]
-
-  routes = [
-    {
-      link_name = "phy0"
-      routes = [
-        {
-          destination = "0.0.0.0/0"
-          gateway     = "172.20.250.1"
-        },
-      ]
-    },
-  ]
-
-  timeouts = {
-    read   = "1m"
-    create = "3m"
-    delete = "2m"
-    update = "4m"
-  }
-}
-`
-
-func checkResourceSwitchPortSettingsRemoveUpdate(resourceName string) resource.TestCheckFunc {
-	return resource.ComposeAggregateTestCheckFunc([]resource.TestCheckFunc{
-		resource.TestCheckResourceAttrSet(resourceName, "id"),
-		resource.TestCheckResourceAttr(resourceName, "name", "test-uplink"),
-		resource.TestCheckResourceAttr(resourceName, "description", "test uplink configuration"),
-		resource.TestCheckResourceAttr(resourceName, "port_config", "qsfp28x1"),
+		resource.TestCheckResourceAttr(resourceName, "addresses.#", "1"),
 		resource.TestCheckResourceAttr(resourceName, "addresses.0.link_name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address", "172.20.250.10/24"),
-		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address_lot.name", "test-address-lot"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.autoneg", "false"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.fec", "none"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.mtu", "1500"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.speed", "speed40_g"),
-		resource.TestCheckResourceAttr(resourceName, "links.0.lldp.enabled", "false"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.link_name", "phy0"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.destination", "0.0.0.0/0"),
-		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.gateway", "172.20.250.1"),
+		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "addresses.0.addresses.0.address", "0.0.0.0/0"),
+		resource.TestCheckResourceAttrSet(resourceName, "addresses.0.addresses.0.address_lot_id"),
+		resource.TestCheckNoResourceAttr(resourceName, "addresses.0.addresses.0.vlan_id"),
 
-		resource.TestCheckResourceAttrSet(resourceName, "time_created"),
-		resource.TestCheckResourceAttrSet(resourceName, "time_modified"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.read", "1m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.delete", "2m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.create", "3m"),
-		resource.TestCheckResourceAttr(resourceName, "timeouts.update", "4m"),
+		resource.TestCheckResourceAttr(resourceName, "links.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.link_name", "phy0"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.autoneg", "false"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.fec"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.mtu", "1500"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.speed", "speed1_g"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.lldp.%", "7"),
+		resource.TestCheckResourceAttr(resourceName, "links.0.lldp.enabled", "true"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.chassis_id"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.link_description"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.link_name"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.management_ip"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.system_description"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.lldp.system_name"),
+		resource.TestCheckNoResourceAttr(resourceName, "links.0.tx_eq"),
+
+		resource.TestCheckResourceAttr(resourceName, "routes.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.link_name", "phy0"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.#", "1"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.dst", "0.0.0.0/0"),
+		resource.TestCheckResourceAttr(resourceName, "routes.0.routes.0.gw", "0.0.0.0"),
+		resource.TestCheckNoResourceAttr(resourceName, "routes.0.routes.0.rib_priority"),
 	}...)
 }
 
@@ -319,22 +270,17 @@ func testAccSwitchPortSettingsDestroy(s *terraform.State) error {
 
 		ctx := context.Background()
 
-		res, err := client.IpPoolView(
+		res, err := client.NetworkingSwitchPortSettingsView(
 			ctx,
-			oxide.IpPoolViewParams{Pool: "terraform-acc-myippool"},
-		)
-		if err == nil || !is404(err) {
-			return fmt.Errorf("ip_pool (%v) still exists", &res.Name)
-		}
-
-		res2, err := client.IpPoolView(
-			ctx,
-			oxide.IpPoolViewParams{Pool: "terraform-acc-myippool2"},
+			oxide.NetworkingSwitchPortSettingsViewParams{
+				Port: oxide.NameOrId(rs.Primary.Attributes["id"]),
+			},
 		)
 		if err != nil && is404(err) {
 			continue
 		}
-		return fmt.Errorf("ip_pool (%v) still exists", &res2.Name)
+
+		return fmt.Errorf("switch_port_settings (%v) still exists", &res.Name)
 	}
 
 	return nil
