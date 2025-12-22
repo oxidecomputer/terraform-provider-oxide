9edf90a6db013739ec4713699d9940c4a407cbf8
Matthew Sanabria <matthew.sanabria@oxide.computer>
2025-12-22 10:47:41 -0500

    fix: formatting

--- examples/provider/provider-auth-config.tf
+++ examples/provider/provider-auth-config.tf
@@ -3,6 +3,7 @@ variable "oxide_token" {
   description = "Oxide API token."
   sensitive   = true
 }
+
 provider "oxide" {
   host  = "https://oxide.sys.example.com"
   token = "oxide-token-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
