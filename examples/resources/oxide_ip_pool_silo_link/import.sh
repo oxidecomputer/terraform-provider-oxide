9e8fbbd5345cd0c765774c66c97ae25fb6bde4cb
Matthew Sanabria <matthew.sanabria@oxide.computer>
2026-06-30 16:21:40 -0400

    oxide_ip_pool_silo_link: use composite attributes for id
    The `id` attribute for the `oxide_ip_pool_silo_link` resource
    now uses the format `IP_POOL_ID/SILO_ID`, matching the
    `oxide_subnet_pool_silo_link` resource.
    
    Closes SSE-348.

--- examples/resources/oxide_ip_pool_silo_link/import.sh
+++ examples/resources/oxide_ip_pool_silo_link/import.sh
@@ -1,1 +1,2 @@
-terraform import oxide_ip_pool_silo_link.example 3e2c6e84-bed8-4c94-afc3-1032082d6a90
+# Import ID is the format `${IP_POOL_ID}/${SILO_ID}`.
+terraform import oxide_ip_pool_silo_link.example 3e2c6e84-bed8-4c94-afc3-1032082d6a90/9e199e45-01a6-43d3-8bc3-5b27726e67a6
